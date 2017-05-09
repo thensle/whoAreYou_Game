e6114e42defe20c30222b940861af4c79bda268c
foxmessenger <iamliamfox@gmail.com>
2017-05-09 18:56:53 -0500

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- NSFW 0
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever been arrested", 0);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever sharted", 0);
+INSERT INTO questions (question, sfw) VALUES ("Have you ever though of your teacher sexually", 0);
+INSERT INTO questions (question, sfw) VALUES ("Have you ever smelled someone else's hair", 0);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever licked a pair of balls", 0);
+
+--SFW 1
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever eaten a ham sandwich", 1);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever worn a dress", 1);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever cut my own hair", 1);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever driven my own car", 1);
+INSERT INTO questions (question, sfw) VALUES ("Never have I ever worried about my weight", 1)
